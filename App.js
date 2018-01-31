c5e78b6a37e063f0a082e752fb46b2e76e1373e6
Cecily <cecily.downs@gmail.com>
2018-01-31 13:51:22 -0800

    Insert ProgressCircle component

--- App.js
+++ App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import ProgressCircle from 'react-native-progress-circle'
+
 
 export default class App extends React.Component {
   render() {
@@ -8,6 +10,16 @@ export default class App extends React.Component {
         <Text>Open up App.js to start working on your app!</Text>
         <Text>Changes you make will automatically reload.</Text>
         <Text>Shake your phone to open the developer menu.</Text>
+        <ProgressCircle
+            percent={30}
+            radius={80}
+            borderWidth={8}
+            color="#3399FF"
+            shadowColor="#999"
+            bgColor="#fff"
+        >
+            <Text style={{ fontSize: 18 }}>{'29'}</Text>
+        </ProgressCircle>
       </View>
     );
   }
