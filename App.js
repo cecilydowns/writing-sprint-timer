223ab9709ece71c458280e88d02ca95c2b35a43d
Cecily <cecily.downs@gmail.com>
2018-01-31 15:33:31 -0800

    Add and style form input, and hook up customization of timer length

--- App.js
+++ App.js
@@ -1,22 +1,40 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 import ProgressCircle from 'react-native-progress-circle'
 import CountdownTimer from './components/CountdownTimer'
 
 export default class App extends React.Component {
 
-  startTimer = () => {
+  constructor(){
+    super()
+    this.state = {
+      timeSelected: "15"
+    }
+  }
 
+  onChange = (text) => {
+    // code to remove non-numeric characters from text
+    this.setState({timeSelected: text})
   }
 
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+      <View style={styles.container}>       
 
-        <CountdownTimer secondsRemaining={16} />
+        <View style={styles.formContainer}>
+          <Text>I would like to write for</Text>
+          <TextInput 
+            style={styles.timeInput}
+            keyboardType = 'numeric'
+            onChangeText = {(text)=> this.onChange(text)}
+            value = {this.state.timeSelected}
+            placeHolder="Select a time"
+          /> 
+          <Text>minutes</Text>
+      </View>
+        <Text>{this.state.timeSelected}</Text>
+        
+        <CountdownTimer secondsRemaining={this.state.timeSelected} />
 
       </View>
     );
@@ -26,8 +44,19 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center'
   },
+  formContainer: {
+
+  },
+  timeInput: {
+    height: 60,
+    width: 60,
+    borderColor: 'grey',
+    borderWidth: 1,
+    fontSize: 25,
+    color: 'grey',
+    fontWeight: 'bold'
+  }
 });
