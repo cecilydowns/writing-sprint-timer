ed50ea88f941fdf6aff711a678804376f6c81596
Cecily <cecily.downs@gmail.com>
2018-02-12 15:46:57 -0800

    Add store.js file to set up redux store

---
+++ app/redux/store.js
@@ -0,0 +1,10 @@
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+
+import reducers from './rootReducer'; //Import the root reducer
+
+const enhancer = compose(applyMiddleware(thunk));
+
+// Connect our store to the reducers
+export default createStore(reducers, enhancer);
+
