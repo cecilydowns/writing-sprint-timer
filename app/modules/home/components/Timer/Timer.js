22841da66cd403cd3d361ac8bb26c72e75f5c3f2
Cecily <cecily.downs@gmail.com>
2018-02-21 13:55:54 -0800

    Add files and basic code for Timer component

---
+++ app/modules/home/components/Timer/Timer.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Text, View, TouchableOpacity } from 'react-native';
+import styles from './styles';
+
+class Timer extends React.Component {
+
+    render() {
+        return (
+            <View>
+                <Text>Here's the timer!</Text>
+            </View>
+        );
+    }
+}
+
+
+export default Timer
