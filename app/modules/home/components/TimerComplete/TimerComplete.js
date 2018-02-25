e1971b20b66c598de8734491122ca76a66c6eca3
Cecily <cecily.downs@gmail.com>
2018-02-25 11:39:08 -0800

    Move timer complete button to new TimerCompleteForm component

--- app/modules/home/components/TimerComplete/TimerComplete.js
+++ app/modules/home/components/TimerComplete/TimerComplete.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Text, View, TouchableOpacity, Button } from 'react-native';
+import { FormLabel, FormInput } from 'react-native-elements'
 import styles from './styles';
 
 class TimerComplete extends React.Component {
@@ -7,12 +8,11 @@ class TimerComplete extends React.Component {
     render() {
         return (
             <View>
-                <Text>You did it!!!!</Text>
-                <Button title="reset..." onPress={() => {this.props.onTimerSave(60, 1543)}} />
+                <Text>You did it!!!! Wrote for {this.props.time} minutes</Text>
             </View>
         );
     }
 }
 
 
-export default TimerComplete
+export default TimerComplete;
