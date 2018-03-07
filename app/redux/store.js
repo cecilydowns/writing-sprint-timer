ee1645e3b38565c71fdb2ddb95381b9d0ad86d73
Cecily <cecily.downs@gmail.com>
2018-03-07 14:38:29 -0800

    Add redux dev tools to store

--- app/redux/store.js
+++ app/redux/store.js
@@ -1,10 +1,14 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 
+import { composeWithDevTools } from 'remote-redux-devtools'
+
+import devToolsEnhancer from 'remote-redux-devtools'
+
 import reducers from './rootReducer'; //Import the root reducer
 
-const enhancer = compose(applyMiddleware(thunk));
+const enhancer = composeWithDevTools(applyMiddleware(thunk));
+
 
 // Connect our store to the reducers
 export default createStore(reducers, enhancer);
-
