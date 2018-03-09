02076b924681c147f72ab6bd07f94257fe1f7a75
Cecily <cecily.downs@gmail.com>
2018-03-09 12:24:43 -0800

    Add header component with placeholder title

---
+++ app/modules/home/components/Header/styles.js
@@ -0,0 +1,18 @@
+import { StyleSheet } from 'react-native';
+
+
+const styles = StyleSheet.create({
+    headerContainer:{
+        height: 90,
+        backgroundColor: "white",
+        paddingTop: 45
+    },
+    siteTitle: {
+        fontFamily: "BerkshireSwash",
+        fontSize: 25,
+        textAlign: 'center'
+    }
+
+});
+
+export default styles;
