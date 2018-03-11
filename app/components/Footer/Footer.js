4d9855aecd909296bd34990b8d28ff560c6a64c6
Cecily <cecily.downs@gmail.com>
2018-03-11 15:12:29 -0700

    Add working links to footer

--- app/components/Footer/Footer.js
+++ app/components/Footer/Footer.js
@@ -4,15 +4,18 @@ import { View, Text, ActivityIndicator, Image } from 'react-native';
 import styles from './styles'
 import FooterItem from './FooterItem'
 
+import { Actions } from 'react-native-router-flux';
+
+
 export default class extends React.Component {
     render() {
         return (
             <View style={styles.container}>
                 <View style={styles.wrapper}>
-                    <FooterItem icon='home' text='Home' />
-                    <FooterItem icon='book' text='Projects' />
-                    <FooterItem icon='insert-chart' text='Stats' />
-                    <FooterItem icon='settings' text='Settings' />
+                    <FooterItem icon='home' text='Home' click={Actions.Main} />
+                    <FooterItem icon='book' text='Projects' click={Actions.Projects} />
+                    <FooterItem icon='insert-chart' text='Stats' click={Actions.Settings} />
+                    <FooterItem icon='settings' text='Settings' click={Actions.Settings} />
                 </View>
             </View>
         );
