281b7075cae455e905321853dd71ff41532a6c14
Cecily <cecily.downs@gmail.com>
2018-04-26 10:53:03 -0700

    Adjust spacing in auth actions, and ensure correct data is stored in AsyncStorage and in redux on user login

--- app/modules/auth/actions.js
+++ app/modules/auth/actions.js
@@ -1,89 +1,84 @@
-import * as t from './actionTypes';
-import * as api from './api';
+import * as t from "./actionTypes";
+import * as api from "./api";
 import firebase from "../../config/firebase";
 
-import { AsyncStorage } from 'react-native';
+import { AsyncStorage } from "react-native";
 
 export function register(user, successCB, errorCB) {
-    return (dispatch) => {
-        api.register(user, function (success, data, error) {
-            if (success) {
-                dispatch({type: t.LOGGED_IN, data: user});
-                AsyncStorage.setItem('user', JSON.stringify(user))
-                AsyncStorage.setItem('jwt', data.token)
-                successCB();
-            } else if (error) errorCB(error)
-        });
-    };
+  return dispatch => {
+    api.register(user, function(success, data, error) {
+      if (success) {
+        dispatch({ type: t.LOGGED_IN, data: user });
+        AsyncStorage.setItem("user", JSON.stringify(user));
+        AsyncStorage.setItem("jwt", data.token);
+        successCB();
+      } else if (error) errorCB(error);
+    });
+  };
 }
 
-
-
 export function login(user, successCB, errorCB) {
-    return (dispatch) => {
-        api.login(user, function (success, data, error) {
-            if (success) {
-                console.log("LOGIN ACTION")
-                console.log("Data:")
-                console.log(data)
-                console.log("User:")
-                console.log(user)
-                if (data.exists) dispatch({type: t.LOGGED_IN, data: user});
-                AsyncStorage.setItem('user', JSON.stringify(user))
-                AsyncStorage.setItem('jwt', data.token)
-                successCB(data);
-            }else if (error) errorCB(error)
-        });
-    };
+  return dispatch => {
+    api.login(user, function(success, data, error) {
+      if (success) {
+        console.log("LOGIN ACTION");
+        console.log("Data:");
+        console.log(data);
+        console.log("User:");
+        console.log(user);
+        if (data.exists) dispatch({ type: t.LOGGED_IN, data: data.user });
+        AsyncStorage.setItem("user", JSON.stringify(data.user));
+        AsyncStorage.setItem("jwt", data.token);
+        successCB(data);
+      } else if (error) errorCB(error);
+    });
+  };
 }
 
 export function resetPassword(data, successCB, errorCB) {
-    return (dispatch) => {
-        api.resetPassword(data, function (success, data, error) {
-            if (success) successCB();
-            else if (error) errorCB(error)
-        });
-    };
+  return dispatch => {
+    api.resetPassword(data, function(success, data, error) {
+      if (success) successCB();
+      else if (error) errorCB(error);
+    });
+  };
 }
 
 export function signOut(successCB, errorCB) {
-    return (dispatch) => {
+  return dispatch => {
+    dispatch({ type: t.LOGGED_OUT });
+    AsyncStorage.multiRemove(["user", "jwt"], err => {
+      if (err) {
+        errorCB();
+      } else {
+        successCB();
+      }
+    });
 
-        dispatch({type: t.LOGGED_OUT})
-        AsyncStorage.multiRemove(['user', 'jwt'], (err) => {
-            if(err){
-                errorCB()
-            } else {
-                successCB()
-            }
-        })
-
-        // api.signOut(function (success, data, error) {
-        //     if (success) {
-        //         dispatch({type: t.LOGGED_OUT});
-        //         successCB();
-        //     }else if (error) errorCB(error)
-        // });
-    };
+    // api.signOut(function (success, data, error) {
+    //     if (success) {
+    //         dispatch({type: t.LOGGED_OUT});
+    //         successCB();
+    //     }else if (error) errorCB(error)
+    // });
+  };
 }
 
 export function checkLoginStatus(callback) {
-    return (dispatch) => {
+  return dispatch => {
+    // So what do I actually want to do here?
+    //get the user object from the Async storage
 
-        // So what do I actually want to do here?
-        //get the user object from the Async storage
+    AsyncStorage.getItem("user", (err, user) => {
+      if (user === null) {
+        dispatch({ type: t.LOGGED_OUT });
+        callback(false);
+      } else {
+        dispatch({ type: t.LOGGED_IN, data: JSON.parse(user) });
+        callback(true);
+      }
+    });
 
-        AsyncStorage.getItem('user', (err, user) => {
-            if (user === null) {
-                dispatch({type: t.LOGGED_OUT});
-                callback(false);
-            } else {
-                dispatch({type: t.LOGGED_IN, data: JSON.parse(user)})
-                callback(true);
-            } 
-        });
-        
-                // callback(isLoggedIn);
-    }    
-
+    // callback(isLoggedIn);
+  };
 }
